5a0d6fd61729bda3aa3226064612503b893f8bc0
Sabrie Sevil Hatipoglu <sevilhatipogluu93@gmail.com>
2022-01-19 22:02:53 +0300

    Added react compoenent for Expense Item

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$298.43</div>
+      </div>
+    </>
+  );
+}
+
+export default ExpenseItem;
